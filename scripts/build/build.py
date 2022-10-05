bbe02be54aebdc39b8e0347043b6a51938d0c1c9
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-05 01:20:05 +0000

    Adding numeric_owner as keyword arguement

--- scripts/build/build.py
+++ scripts/build/build.py
@@ -34,7 +34,7 @@ def build(build_dir, out_dir):
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(tar, build_dir)
